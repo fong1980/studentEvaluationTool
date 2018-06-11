e72d265df474c3469dd49525a424c7c56da2bbe8
fong1980 <fong1980@gmail.com>
2018-06-11 15:42:33 +0200

    add batch succeed

--- server/src/batchs/controller.ts
+++ server/src/batchs/controller.ts
@@ -1,20 +1,43 @@
-import { JsonController, Get } from "routing-controllers";
-import batch from "./entity";
+import { JsonController, Get, Post, HttpCode, Body } from "routing-controllers";
+import Batch from "./entity";
 
 @JsonController()
 export default class batchsController {
   @Get("/batchs")
   async allBatchs() {
-    const batchs = await batch.find();
+    const batchs = await Batch.find();
     return { batchs };
   }
+
+  //http get :4000/batchs
+
+  //   @Post("/batch")
+  //   @HttpCode(201)
+  //   createbatch(@Body() batch: Batch) {
+  //     return batch.save();
+  //   }
+
+  @Post("/batch")
+  @HttpCode(201)
+  async newQuestion(@Body() batch: Batch) {
+    return batch.save();
+  }
+
+  // http post :4000/batch/ batchnr=10 startdate="22-04-2019" enddate="22-06-2019"
 }
 
-//http get :4000/batchs
-//http put :4000/games/7 name=newname color=blue
-// http put :4000/games/7 name=newname color=nocolor
-// http put :4000/games/2 name=newname color=nocolor board='[[\'o\', 'o', 'o'],['o', 'o', 'o'],['o', '1', 'o']]'
-//http put :4000/games/2 name=newname color=blue board='{['o', 'o', 'o'],['o', 'o', 'o'],['o', '1', 'o']}'
-//[['o', 'o', 'o'],['o', 'o', 'o'],['o', '1', 'o']]
+// @Post("/questions")
+// @HttpCode(201)
+// async newQuestion(@Body() questions: Questions) {
+//   return questions.save();
+// }
 
-// http post :4000/games/2 name=newname color=nocolor board=
+// @Post('/games')
+// @HttpCode(201)
+// creategame(
+// @Body() game: Game
+// ) {
+//     game.color=colors[Math.floor(Math.random()*colors.length)];
+//     game.board=defaultBoard
+// return game.save()
+// }
