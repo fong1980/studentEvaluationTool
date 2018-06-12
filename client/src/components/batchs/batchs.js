5fd314397d6bd4dea6b1d70dc778d1d966fe1ef0
fong1980 <fong1980@gmail.com>
2018-06-12 19:24:50 +0200

    problems with first load of page

--- client/src/components/batchs/batchs.js
+++ client/src/components/batchs/batchs.js
@@ -4,22 +4,35 @@ import { connect } from "react-redux";
 import { getUsers } from "../../actions/users";
 import { getBatchs } from "../../actions/batchs";
 
+const a = ["adfa", "dfad", "asdfad"];
 class batchs extends PureComponent {
   componentWillMount() {
     if (this.props.authenticated) {
-      if (this.props.batchs === null) this.props.getBatchs();
+      if (this.props.batchs === null) this.props.getBatchs(); //=== null
     }
   }
 
   render() {
     const { authenticated } = this.props; //games, users,createGame deleted
+    const { batchs } = this.props;
+
+    //console.log(this.props.batchs);
+    //console.log(typeof this.props.batchs);
+
+    // console.log(object1);
+    // console.log(typeof object1);
+    // console.log(Object.values(object1));
 
     if (!authenticated) return <Redirect to="/login" />;
+    if (!batchs) console.log("nee man");
 
     return (
       <div>
         <div className="all batch">batchlist</div>
-        <div className="form">form</div>
+        {console.log(batchs, "test")}
+        {a.map(x => "x")}
+
+        <div className="form">-</div>
       </div>
     );
   }
