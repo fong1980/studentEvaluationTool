624884729f7e6005799d0a732c0ae81237fd0e14
fong1980 <fong1980@gmail.com>
2018-06-13 13:49:15 +0200

    fetch succeed, now make student component

--- client/src/actions/student.js
+++ client/src/actions/student.js
@@ -5,30 +5,30 @@ import { isExpired } from "../jwt";
 
 export const GET_STUDENT = "GET_STUDENT";
 
-export const getStudent = studId => {
-  console.log("action--");
-  return {
-    type: GET_STUDENT,
-    payload: studId
-  };
-};
-
-// export const getStudents = batchId => (dispatch, getState) => {
-//   //change functionname + parameter
-//   const state = getState();
-//   if (!state.currentUser) return null;
-//   const jwt = state.currentUser.jwt;
-//   if (isExpired(jwt)) return dispatch(logout());
-
-//   request
-//     .get(`${baseUrl}/studentBatch/${batchId}`)
-//     .then(response =>
-//       dispatch({
-//         type: GET_STUDENTS, //change type
-//         payload: response.body
-//       })
-//     )
-//     .catch(err => alert(err));
+// export const getStudent = studId => {
+//   console.log("action--");
+//   return {
+//     type: GET_STUDENT,
+//     payload: studId
+//   };
 // };
 
-// //http get :4000/studentBatch/2
+export const getStudent = studId => (dispatch, getState) => {
+  //change functionname + parameter
+  const state = getState();
+  if (!state.currentUser) return null;
+  const jwt = state.currentUser.jwt;
+  if (isExpired(jwt)) return dispatch(logout());
+
+  request
+    .get(`${baseUrl}/students/${studId}`)
+    .then(response =>
+      dispatch({
+        type: GET_STUDENT, //change type
+        payload: response.body
+      })
+    )
+    .catch(err => alert(err));
+};
+
+//http get :4000/students/2
